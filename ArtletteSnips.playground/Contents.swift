32a66b27abb84915ccf855e74ff9eb483232f1a3
Nicole Lehrer <nicole.lehrer@gmail.com>
2015-12-17 16:21:45 -0600

    shapeViews are animated after being added to the view to produce a card deck effect

--- ArtletteSnips.playground/Contents.swift
+++ ArtletteSnips.playground/Contents.swift
@@ -3,22 +3,27 @@
 import UIKit
 import XCPlayground
 
-let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
+let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 667, height: 375))
 containerView.backgroundColor = UIColor.whiteColor()
 XCPlaygroundPage.currentPage.liveView = containerView
 
 
 func addSubViews(size:CGSize, offset:CGPoint){
-    for i in 0...4{
+    for i in 0...9{
         let blueValue = CGFloat(Int(arc4random() % 255)) / 255.0
         let greenValue = CGFloat(Int(arc4random() % 255)) / 255.0
         let redValue = CGFloat(Int(arc4random() % 255)) / 255.0
         let someColor = UIColor(red:redValue, green: greenValue, blue: blueValue, alpha: 1.0)
-        let shapeView = ShapeView(frame: CGRect(x: CGFloat(i)*size.width/2 + offset.x, y: offset.y, width: size.width, height: size.height), color: someColor)
+        
+        let shapeView = ShapeView(frame: CGRect(x: offset.x, y: offset.y, width: size.width, height: size.height), color: someColor)
         containerView.addSubview(shapeView)
+        
+        let newOriginX = offset.x + CGFloat(i)*size.width/2
+        
+        shapeView.animateOrigin(CGPoint(x: newOriginX, y: offset.y), delay: 0.0, duration: 0.15*Double(i), completion:nil)
     }
 }
 
 
 addSubViews(CGSize(width:80, height:80), offset: CGPoint(x: 20, y: 20))
-print("hello")
+
