35a2a532f6af5cde91372fb195c51a417a444500
Nicole Lehrer <nicole.lehrer@gmail.com>
2015-12-17 12:30:07 -0600

    added a container view to draw something

--- ArtletteSnips.playground/Contents.swift
+++ ArtletteSnips.playground/Contents.swift
@@ -1,5 +1,14 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCPlayground
 
-var str = "Hello, playground"
+let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
+containerView.backgroundColor = UIColor.whiteColor()
+XCPlaygroundPage.currentPage.liveView = containerView
+
+let view = UIView(frame:CGRect(x:0, y:0, width:100, height:100))
+view.backgroundColor = UIColor.blackColor()
+containerView.addSubview(view)
+
+print("heelo")
